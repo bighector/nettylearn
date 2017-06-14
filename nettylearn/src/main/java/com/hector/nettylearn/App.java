bc0518b5189d2c79b615affae34ea247b52b21b8
Administrator <Administrator@WIN-UM77UNNOUHL>
2017-06-14 20:48:37 +0800

    begin

---
+++ nettylearn/src/main/java/com/hector/nettylearn/App.java
@@ -0,0 +1,13 @@
+package com.hector.nettylearn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
